b415146f56e187d5abb9c7035f09578ec34b30d9
HyeranShin <hyeran9712@naver.com>
2019-07-19 15:47:09 +0900

    프로젝트 생성

---
+++ src/main/java/com/soma/dodam/dodami/DodamiApplication.java
@@ -0,0 +1,13 @@
+package com.soma.dodam.dodami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodamiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodamiApplication.class, args);
+    }
+
+}
