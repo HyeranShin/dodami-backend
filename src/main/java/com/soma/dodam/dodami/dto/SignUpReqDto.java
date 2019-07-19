677c6849503f84a4eed0aad7fa6b4bf85da19c82
HyeranShin <hyeran9712@naver.com>
2019-07-19 21:06:41 +0900

    SignUpReqDto 유효성 검사 추가

--- src/main/java/com/soma/dodam/dodami/dto/SignUpReqDto.java
+++ src/main/java/com/soma/dodam/dodami/dto/SignUpReqDto.java
@@ -4,23 +4,39 @@ import com.soma.dodam.dodami.domain.User;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Pattern;
+
 @Getter
 public class SignUpReqDto {
+    private final static String ID_REGEX = "^[a-z0-9]{4,12}$";
+    private final static String PASSWORD_REGEX = "^[a-zA-Z0-9]{8,20}$";
+    private final static String NAME_REGEX = "^[가-힣]{2,}$";
+    private final static String PHONE_REGEX = "^01[0|1|6-9][0-9]{3,4}[0-9]{4}$";
 
-    @ApiModelProperty(notes = "아이디", example = "dodami", position = 1)
+    @ApiModelProperty(notes = "4~12자의 영문 소문자나 숫자", example = "dodami", position = 1)
+    @Pattern(regexp = ID_REGEX, message = "아이디 형식이 올바르지 않습니다.")
     private String id;
 
-    @ApiModelProperty(notes = "비밀번호", example = "dodami123", position = 2)
+    @ApiModelProperty(notes = "8~20자의 영어 대소문자나 숫자", example = "dodami123", position = 2)
+    @Pattern(regexp = PASSWORD_REGEX, message = "비밀번호 형식이 올바르지 않습니다.")
     private String password;
 
-    @ApiModelProperty(notes = "비밀번호 확인", example = "dodami123", position = 3)
+    @ApiModelProperty(notes = "password와 일치", example = "dodami123", position = 3)
     private String configPassword;
 
-    @ApiModelProperty(notes = "이름", example = "도담이", position = 4)
+    @ApiModelProperty(notes = "2자 이상의 한글", example = "도담이", position = 4)
+    @Pattern(regexp = NAME_REGEX, message = "이름 형식이 올바르지 않습니다.")
     private String name;
 
-    @ApiModelProperty(notes = "휴대폰 번호", example = "010-1234-1234", position = 5)
+    @ApiModelProperty(notes = "\n", example = "010-1234-1234", position = 5)
+    @Pattern(regexp = PHONE_REGEX, message = "휴대폰 반호 형식이 올바르지 않습니다.")
     private String phone;
+
+    @AssertTrue(message = "비밀번호와 비밀번호 확인이 일치하지 않습니다.")
+    private boolean isEualConfigPassword() {
+        return password.equals(configPassword);
+    }
 
     public User toUser() {
         return User.builder()
--- src/main/java/com/soma/dodam/dodami/web/UserController.java
+++ src/main/java/com/soma/dodam/dodami/web/UserController.java
@@ -5,6 +5,7 @@ import com.soma.dodam.dodami.dto.SignUpReqDto;
 import com.soma.dodam.dodami.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @ApiOperation(value = "회원 가입", notes = "")
+    @ApiOperation(value = "회원 가입", notes = "유효성 검사를 통과해야 합니다. 하단의 Model을 참고하세요.")
     @PostMapping("")
-    public String signUp(@RequestBody SignUpReqDto signUpReqDto) {
+    public String signUp(@Validated @RequestBody SignUpReqDto signUpReqDto) {
         User user = userService.signUp(signUpReqDto);
         return user.getName()+"님 회원 가입 성공";
     }
