912026d5d19f852fdf56499c552c02c23c306eb0
HyeranShin <hyeran9712@naver.com>
2019-09-03 13:20:44 +0900

    테스트 주석 제거

--- src/test/java/com/soma/dodam/dodami/DodamiApplicationTests.java
+++ src/test/java/com/soma/dodam/dodami/DodamiApplicationTests.java
@@ -1,16 +1,16 @@
-//package com.soma.dodam.dodami;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class DodamiApplicationTests {
-//
-////    @Test
-////    public void contextLoads() {
-////    }
-//
-//}
+package com.soma.dodam.dodami;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DodamiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
